100b720a992b328cec08bb8c6cc3ab18dfb56d51
Kieran Gibb <kg@magmacollective.org>
2020-07-27 12:16:09 +0100

    run only new-basic tests

--- test/new-basic.js
+++ test/new-basic.js
@@ -4,7 +4,44 @@ const MultifeedNetworker = require('../networker')
 const Multifeed = require('..')
 const { create, cleanup } = require('./lib/networker')
 
-test.only('multiple topics example', async function (t) {
+test('minimal swarm example', async function (t) {
+  const { store: store1, networker: networker1 } = await create()
+  const { store: store2, networker: networker2 } = await create()
+
+  const swarm1 = new MultifeedNetworker(networker1)
+  const swarm2 = new MultifeedNetworker(networker2)
+
+  const rootKey = hcrypto.keyPair().publicKey
+
+  const multi1 = new Multifeed(store1, { rootKey })
+  swarm1.swarm(multi1)
+  await ready(multi1)
+
+  const multi2 = new Multifeed(store2, { rootKey })
+  swarm2.swarm(multi2)
+  await ready(multi2)
+
+  const writer1 = await writer(multi1, 'foo')
+  const writer2 = await writer(multi2, 'bar')
+  await append(writer1, 'first')
+  await append(writer2, 'second')
+
+  await timeout(500)
+
+  t.deepEqual(toKeys(multi1.feeds()), toKeys([writer1, writer2]))
+  t.deepEqual(toKeys(multi2.feeds()), toKeys([writer1, writer2]))
+  t.deepEqual(
+    await get(await writer(multi1, '1'), 0),
+    Buffer.from('second')
+  )
+  t.deepEqual(
+    await get(await writer(multi2, '1'), 0),
+    Buffer.from('first')
+  )
+  await cleanup([networker1, networker2])
+})
+
+test('multiple topics example', async function (t) {
   const { store: store1, networker: networker1 } = await create()
   const { store: store2, networker: networker2 } = await create()
 
@@ -71,55 +108,6 @@ test.only('multiple topics example', async function (t) {
   await cleanup([networker1, networker2])
 })
 
-function toKeys (feeds) {
-  return feeds.map(f => f.key.toString('hex')).sort()
-}
-
-function ready (resource) {
-  return new Promise((resolve, reject) => {
-    resource.ready(err => {
-      if (err) return reject(err)
-      resolve()
-    })
-  })
-}
-
-test('minimal swarm example', async function (t) {
-  const { store: store1, networker: networker1 } = await create()
-  const { store: store2, networker: networker2 } = await create()
-
-  const swarm1 = new MultifeedNetworker(networker1)
-  const swarm2 = new MultifeedNetworker(networker2)
-
-  const rootKey = hcrypto.keyPair().publicKey
-
-  const multi1 = new Multifeed(store1, { rootKey })
-  swarm1.swarm(multi1)
-  await ready(multi1)
-
-  const multi2 = new Multifeed(store2, { rootKey })
-  swarm2.swarm(multi2)
-  await ready(multi2)
-
-  const writer1 = await writer(multi1, 'foo')
-  const writer2 = await writer(multi2, 'bar')
-  await append(writer1, 'first')
-  await append(writer2, 'second')
-
-  await timeout(500)
-
-  t.deepEqual(toKeys(multi1.feeds()), toKeys([writer1, writer2]))
-  t.deepEqual(toKeys(multi2.feeds()), toKeys([writer1, writer2]))
-  t.deepEqual(
-    await get(await writer(multi1, '1'), 0),
-    Buffer.from('second')
-  )
-  t.deepEqual(
-    await get(await writer(multi2, '1'), 0),
-    Buffer.from('first')
-  )
-  await cleanup([networker1, networker2])
-})
 
 function toKeys (feeds) {
   return feeds.map(f => f.key.toString('hex')).sort()
